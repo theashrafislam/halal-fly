19a0105a774489210860cf337e7bb2d1aa3b3843
Ashraful Islam <theashrafislam@gmail.com>
2025-04-09 00:50:44 +0600

    card section reponsive for all devices

--- src/app/page.js
+++ src/app/page.js
@@ -16,10 +16,10 @@ export default function Home() {
       <HeroSection />
 
       {/* card section  */}
-      {/* <CardSection /> */}
+      <CardSection />
 
       {/* FindTraval */}
-      {/* <FindTraval /> */}
+      <FindTraval />
 
       {/* Destinations */}
       {/* <Destinations /> */}
