157bee6827474ecd88a3976c4f1d21b15f2ad1ae
Ashraful Islam <theashrafislam@gmail.com>
2025-04-09 01:00:03 +0600

    destinations compoment make respoinsive for all devices

--- src/app/Components/Destinations/Destinations.jsx
+++ src/app/Components/Destinations/Destinations.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 
 const Destinations = () => {
     return (
-        <div className='pt-[163px] pb-[120px] bg-[#1A5F7A] text-[#F5F5F5]'>
+        <div className='pt-[120px] md:pt-[163px] pb-[120px] min-h-screen bg-[#1A5F7A] text-[#F5F5F5]'>
 
             <div className='max-w-7xl mx-auto'>
                 <div className='pb-[57px] text-center  mx-3 lg:mx-0'>
-                    <h4 className='text-5xl font-bold mb-3'>Popular Destinations</h4>
+                    <h4 className='text-4xl md:text-5xl font-bold mb-3'>Popular Destinations</h4>
                     <p className='text-base'>An enim nullam tempor gravida donec enim  congue magna at pretium purus pretium ligula <br /> rutrum luctus risusd diam eget risus varius blandit sit amet non magna.</p>
                 </div>
+
                 {/* image section  */}
-                <div className='flex flex-col md:flex-row items-center gap-[35px] h-full  mx-3 lg:mx-0'>
+                <div className='flex flex-col md:flex-row items-center gap-[35px] min-h-screen  mx-3 lg:mx-0'>
                     {/* left  */}
                     <div className='relative w-full lg:w-[50%] h-[500px]  '>
                         <Image width={8000} height={5000} alt='hello' className='w-full h-full rounded-xl object-cover' src={'/hero-section.jpg'} />
@@ -31,13 +32,13 @@ const Destinations = () => {
                         </div>
                         <div className='flex flex-col lg:flex-row items-center justify-between gap-[24px]'>
                             <div className='relative '>
-                                <Image width={800} height={500} alt='hello' className='rounded-xl' src={'/hero-section.jpg'} />
+                                <Image width={800} height={400} alt='hello' className='rounded-xl' src={'/hero-section.jpg'} />
                                 <div className='absolute bottom-5 left-5'>
                                     <h3 className='text-2xl font-bold'>Saudi Arabia</h3>
                                     <p className='text-base font-normal pt-3'>15 Tours</p>
                                 </div>
                             </div>
-                            <div className='relative '>
+                            <div className='relative hidden lg:block'>
                                 <Image width={800} height={500} alt='hello' className='rounded-xl' src={'/hero-section.jpg'} />
                                 <div className='absolute bottom-5 left-5'>
                                     <h3 className='text-2xl font-bold'>South America</h3>
--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
       <FindTraval />
 
       {/* Destinations */}
-      {/* <Destinations /> */}
+      <Destinations />
 
       {/* review  */}
       {/* <Review /> */}
