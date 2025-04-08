14b50faae20ea15a7c498cd2386aa52723ae60f9
Ashraful Islam <theashrafislam@gmail.com>
2025-04-08 08:28:39 +0600

    added font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata = {
@@ -20,9 +16,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
-        {children}
+        {/* header section  */}
+        <header></header>
+
+        {/* main section  */}
+        <main>{children}</main>\
+
+        {/* footer section  */}
+        <footer></footer>
       </body>
     </html>
   );
