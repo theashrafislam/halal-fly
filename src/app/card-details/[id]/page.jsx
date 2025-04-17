145a556140926c0ac7d9aac7eec484412533b082
Ashraful Islam <theashrafislam@gmail.com>
2025-04-17 08:25:56 +0600

    details page hero section header part is complate

--- src/app/card-details/[id]/page.jsx
+++ src/app/card-details/[id]/page.jsx
@@ -1,3 +1,4 @@
+import DetailsHero from '@/app/Components/CardDetails/DetailsHero';
 import React from 'react';
 import { LiaAngleRightSolid } from 'react-icons/lia';
 
@@ -5,11 +6,13 @@ const CardDetailsPage = ({ params }) => {
     const { id } = params;
     // console.log(id);
     return (
-        <div className='max-w-7xl mx-auto'>
+        <div className=''>
             {/* route section  */}
-            <div className='py-[27px] flex items-center justify-center lg:justify-start'>
+            <div className='py-[27px] flex items-center justify-center max-w-7xl mx-auto'>
                 <h5 className='flex items-center gap-3 text-base'><span>Home</span><LiaAngleRightSolid /><span>Tour</span><LiaAngleRightSolid /><span>Tour Package</span></h5>
             </div>
+            {/* hero section  */}
+            <DetailsHero />
         </div>
     );
 };
