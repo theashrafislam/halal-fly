a5569f3a80a0f4d6c12084e759c6d3a1a5119217
Ashraful Islam <theashrafislam@gmail.com>
2025-04-17 11:09:38 +0600

    added hightlight

--- src/app/Components/CardDetails/DetailsHero.jsx
+++ src/app/Components/CardDetails/DetailsHero.jsx
@@ -224,6 +224,31 @@ const DetailsHero = () => {
                             After completing Umrah, the journey continues to Madinah, where pilgrims spend time at Masjid al-Nabawi, the resting place of Prophet Muhammad ﷺ. Visitors often pray in Rawdah, explore historical sites such as Quba Mosque, Uhud Mountain, and the Qiblatain Mosque. The tour includes hotel stays, transport between cities, and often meals and group lectures, making it a well-rounded spiritual and educational experience.
                         </p>
                     </div>
+
+                    {/* Makkah Highlights */}
+                    <div className='mt-7 flex flex-col gap-3'>
+                        <h6 className='text-2xl font-bold'>Makkah Highlights</h6>
+                        <div className='flex items-center gap-2'>
+                            <div className='w-3 h-3 rounded-full bg-[#B82525]'></div>
+                            <p className='text-base text-[#444]'><span className='font-semibold'>Masjid al-Haram:</span> Prayers in the holiest mosque in Islam.</p>
+                        </div>
+                        <div className='flex items-center gap-2'>
+                            <div className='w-3 h-3 rounded-full bg-[#B82525]'></div>
+                            <p className='text-base text-[#444]'><span className='font-semibold'>Kaaba:</span> Heart of the Islamic world, focus of every Muslim's prayer.</p>
+                        </div>
+                        <div className='flex items-center gap-2'>
+                            <div className='w-3 h-3 rounded-full bg-[#B82525]'></div>
+                            <p className='text-base text-[#444]'><span className='font-semibold'>Zamzam Well:</span> Drinking and collecting blessed water.</p>
+                        </div>
+                        <div className='flex items-center gap-2'>
+                            <div className='w-3 h-3 rounded-full bg-[#B82525]'></div>
+                            <p className='text-base text-[#444]'><span className='font-semibold'>Jabal al-Noor:</span> Cave of Hira – first revelation site.</p>
+                        </div>
+                        <div className='flex items-center gap-2'>
+                            <div className='w-3 h-3 rounded-full bg-[#B82525]'></div>
+                            <p className='text-base text-[#444]'><span className='font-semibold'>Jabal al-Thawr:</span> Site where Prophet Muhammad ﷺ hid during Hijrah.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
